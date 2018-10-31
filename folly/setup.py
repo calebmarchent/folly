5be12950eec8993358b7f0e0858443ac4e5a68c8
Caleb Marchent <cmarchent@fb.com>
2018-10-31 12:36:04 +0000

    Build executor Cython extension
    setup.py to create folly Cython bindings
    
    Minimum required to get fbthrift to compile in OSS, so only includes
    executor
    
    Not automatically called by CMake (yet)
    
    Test Plan:
    
    Dimensions to conder in test plan:
    * Distro:
      * Ubuntu 16.04
    * Cython?
      * With / without Cython installed
    * Python Version
      * Only test with Python3
    
    Install Python Bindings during Folly install

---
+++ folly/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+# Example calling command:
+# $./setup.py build_ext --inplace
+
+
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+from Cython.Compiler import Options
+import os
+
+Options.fast_fail = True
+
+ext = Extension("folly.executor",
+                sources=['folly/executor.pyx'],
+   include_dirs=[os.getcwd()+"/..", os.getcwd()],
+   libraries=['folly', 'glog', 'double-conversion', 'iberty'])
+
+if not os.path.isdir('folly'):
+    os.makedirs('folly')
+
+for f in ['executor.pxd',
+          'executor.pyx',
+          'futures.pxd',
+          '__init__.pxd',
+          'AsyncioExecutor.h']:
+    if not os.path.exists('folly/'+f):
+        os.symlink('../python/'+f, 'folly/'+f)
+
+setup(name="folly",
+      version='0.0.1',
+      packages=['folly'],
+      package_data={ "": ['*.pxd', '*.h'] },
+      language="c++",
+      zip_safe=False,
+      ext_modules = cythonize([ext],
+                              compiler_directives={'language_level': 3,}))
