df1f194fcd49f9b678e2626cfe1c211681f5e572
Caleb Marchent <cmarchent@fb.com>
2018-11-28 12:48:35 +0000

    It builds...
    To easy to be true?

--- folly/setup.py
+++ folly/setup.py
@@ -12,25 +12,13 @@ import os
 Options.fast_fail = True
 
 ext = Extension("folly.executor",
-                sources=['folly/executor.pyx'],
+   sources=['python/executor.pyx'],
    include_dirs=[os.getcwd()+"/..", os.getcwd()],
    libraries=['folly', 'glog', 'double-conversion', 'iberty'])
 
-if not os.path.isdir('folly'):
-    os.makedirs('folly')
-
-for f in ['executor.pxd',
-          'executor.pyx',
-          'futures.pxd',
-          '__init__.pxd',
-          'AsyncioExecutor.h']:
-    if not os.path.exists('folly/'+f):
-        os.symlink('../python/'+f, 'folly/'+f)
-
 setup(name="folly",
+      ext_package="folly",
       version='0.0.1',
-      packages=['folly'],
-      package_data={ "": ['*.pxd', '*.h'] },
       language="c++",
       zip_safe=False,
       ext_modules = cythonize([ext],
